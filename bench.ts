4a798d31264a95f93c27a5318823c059c661da41
Jupegarnica <juan@geekshubs.com>
2023-12-17 21:43:59 +0100

    Update deno.json and bench.ts

--- bench.ts
+++ bench.ts
@@ -1,3 +1,10 @@
-Deno.bench('gron', () => {
+import { assertEquals } from "https://deno.land/std@0.209.0/assert/assert_equals.ts";
+import { cmd } from "./cmd.ts";
 
+const gron = 'deno run -A cli.ts'
+
+
+Deno.bench('[e2e] gron 1e3.json', async function () {
+    const { code } = await cmd(`${gron} fixtures/1e3.json`);
+    assertEquals(code, 0)
 });
